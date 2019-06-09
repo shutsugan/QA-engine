e7dc5c3b81646203d9c5c04f5fe4aaf64de30716
shutsugan <muhammad@nextmedia.ma>
2019-06-09 22:19:48 +0100

    chore (entry point): initiate db
    add model, request and db to the server context

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+import { dburl } from './config';
+import { startDb, models} from './db';
+
+const db = startDb({ dburl });
+const context = { models, db };
+
 const typeDefs = `
     type Query {
         hello(name: String): String!
@@ -14,7 +20,11 @@ const resolvers = {
 
 const server = new GraphQLServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: request => ({
+        ...request,
+        ...context
+    })
 });
 
 server.start(_ => console.log('Server is up...'));
