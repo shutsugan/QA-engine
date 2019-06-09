f6d5bd2cdd3aa7121a94d9ecfb3801ee7a2a0a8b
shutsugan <muhammad@nextmedia.ma>
2019-06-09 15:42:58 +0100

    chore (entry point): create graphql server boiler-plate

---
+++ index.js
@@ -0,0 +1,20 @@
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+    type Query {
+        hello(name: String): String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: (_, { name }) => `Hello ${name || 'World'}`
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(_ => console.log('Server is up...'));
