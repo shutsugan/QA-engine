3ed158724510fb0be47e7fd02cce27bf7d49b153
shutsugan <muhammad@nextmedia.ma>
2019-06-13 15:24:05 +0100

    chore (entry point): user graphql query, mutation and resolvers

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ import { GraphQLServer } from 'graphql-yoga';
 import { dburl } from './config';
 import { startDb, models} from './db';
 
+import Query from './graphql/resolvers/Query';
+import Mutation from './graphql/resolvers/Mutation';
+import User from './graphql/resolvers/User';
+import Quastion from './graphql/resolvers/Quastion';
+import Answer from './graphql/resolvers/Answer';
+import Vote from './graphql/resolvers/Vote';
+
 const db = startDb({ dburl });
+const typeDefs = `${__dirname}/graphql/schema.graphql`;
+const resolvers = { Query, Mutation, User, Quastion, Answer, Vote};
 const context = { models, db };
-
-const typeDefs = `
-    type Query {
-        hello(name: String): String!
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: (_, { name }) => `Hello ${name || 'World'}`
-    }
-};
 
 const server = new GraphQLServer({
     typeDefs,
