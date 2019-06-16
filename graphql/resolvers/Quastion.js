b19038159b32fcdeb188f7e0b25898285680881a
shutsugan <muhammad@nextmedia.ma>
2019-06-16 15:41:15 +0100

    feat (quastion resolver): add quastion query methods

--- graphql/resolvers/Quastion.js
+++ graphql/resolvers/Quastion.js
@@ -1,4 +1,33 @@
-import { checkById, updateById } from './utils';
+import { updateById } from './utils';
+
+const quastions = async (_, { skip = 0, limit = 10, published = true }, { models }) => {
+  const { Quastion } = models;
+  const options = { published };
+
+  const quastions = await Quastion
+    .find(options, null, { skip, limit })
+    .populate('author answers votes')
+    .sort({ createdAt: 1 });
+  
+  const count = await Quastion.countDocuments(options);
+
+  if (!quastions) throw new Error('Quastions do not exist');
+
+  return {
+    quastions,
+    count
+  }
+};
+
+const quastion = async (_, { id }, { models }) => {
+  const quastion = models.Quastion
+    .findById(id)
+    .populate('author answers votes');
+
+  if (!quastion) throw new Error('Quastion not found!');
+
+  return quastion;
+};
 
 const createQuation = async (_, { input }, { models }) => {
   const { Quastion, User } = models;
@@ -31,9 +60,34 @@ const deleteQuation = async (_, { id }, { models }) => {
 };
 
 export {
+  quastions,
+  quastion,
   createQuation,
   updateQuation,
   deleteQuation
 };
 
-export default {};
+const author = async ({ id }, _, { models }) => {
+  const quastion = models.Quastion
+    .findById(id)
+    .populate('author');
+
+  if (!quastion) throw new Error('Author not found');
+
+  return quastion.author;
+};
+
+const votes = async ({ id }, _, { models }) => {
+  const quastion = models.Quastion
+    .findById(id)
+    .populate('votes');
+
+  if (!quastion) throw new Error('Votes not found');
+
+  return quastion.votes;
+};
+
+export default {
+  author,
+  votes
+};
