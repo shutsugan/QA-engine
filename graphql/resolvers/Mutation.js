def99bd05206d412736b67a331e07e8d83c788ca
shutsugan <muhammad@nextmedia.ma>
2019-07-06 16:41:48 +0100

    chore (query/mutation): pip answers resolvers

--- graphql/resolvers/Mutation.js
+++ graphql/resolvers/Mutation.js
@@ -1,9 +1,13 @@
 import { signup } from './User';
 import { createQuation, updateQuation, deleteQuation } from './Quastion';
+import { createAnswer, updateAnswer, deleteAnswer } from './Answer';
 
 export default {
   signup,
   createQuation,
   updateQuation,
-  deleteQuation
+  deleteQuation,
+  createAnswer,
+  updateAnswer,
+  deleteAnswer
 };
--- graphql/resolvers/Query.js
+++ graphql/resolvers/Query.js
@@ -1,8 +1,11 @@
 import { user } from './User';
 import { quastions, quastion } from './Quastion';
+import { answers, answer } from './Answer';
 
 export default {
   user,
   quastions,
   quastion,
+  answers,
+  answer,
 };
