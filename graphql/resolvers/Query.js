372fd1b9432bc69d034bfe1c23a058a8f604e70f
shutsugan <muhammad@nextmedia.ma>
2019-07-10 16:48:12 +0100

    chore (vote query): add answer/quastion vote queries

--- graphql/resolvers/Query.js
+++ graphql/resolvers/Query.js
@@ -1,6 +1,7 @@
 import { user } from './User';
 import { quastions, quastion } from './Quastion';
 import { answers, answer } from './Answer';
+import { answerVote, quastionVote } from './Vote';
 
 export default {
   user,
@@ -8,4 +9,6 @@ export default {
   quastion,
   answers,
   answer,
+  answerVote,
+  quastionVote
 };
